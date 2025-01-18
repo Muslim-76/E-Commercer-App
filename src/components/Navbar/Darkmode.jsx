bd0b9e622b3cca5c60cedda9089f3d62846e0cfd
Md Muslim <muslimazam970@gmail.com>
2025-01-18 15:16:01 +0530

    Update dark mode functionality and dependencies

--- src/components/Navbar/Darkmode.jsx
+++ src/components/Navbar/Darkmode.jsx
@@ -3,15 +3,12 @@ import LightButton from "../../assets/websites/light-mode-button.png";
 import DarkButton from "../../assets/websites/dark-mode-button.png";
 
 const DarkMode = () => {
-  // Initialize state for theme
   const [theme, setTheme] = useState(
-    localStorage.getItem("theme") || "light"
+    localStorage.getItem("theme") ?? "light"
   );
 
-  const element = document.documentElement; // Get the root element (html)
-
-  // Sync theme with localStorage and DOM
   useEffect(() => {
+    const element = document.documentElement; // Get the root element (html)
     if (theme === "dark") {
       element.classList.add("dark");
       localStorage.setItem("theme", "dark");
@@ -21,7 +18,6 @@ const DarkMode = () => {
     }
   }, [theme]);
 
-  // Toggle theme on button click
   const handleThemeToggle = () => {
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   };
@@ -31,19 +27,19 @@ const DarkMode = () => {
       {/* Light Mode Button */}
       <img
         src={LightButton}
-        alt="Switch to Dark Mode"
+        alt="Dark Mode"
         onClick={handleThemeToggle}
         className={`w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 absolute right-0 z-10 ${
-          theme === "dark" ? "opacity-0" : "opacity-100"
+          theme === "dark" ? "opacity-0 pointer-events-none" : "opacity-100"
         }`}
       />
       {/* Dark Mode Button */}
       <img
         src={DarkButton}
-        alt="Switch to Light Mode"
+        alt="Light Mode"
         onClick={handleThemeToggle}
         className={`w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 ${
-          theme === "light" ? "opacity-0" : "opacity-100"
+          theme === "light" ? "opacity-0 pointer-events-none" : "opacity-100"
         }`}
       />
     </div>
